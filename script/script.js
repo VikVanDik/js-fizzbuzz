74e7b9443332a60384186107e43bda2bd2f4cd8a
VikVanDik <95vlc17@gmail.com>
2023-09-11 16:03:52 +0200

    [add]:bug i

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
-const boxContainer = document.getElementById('box-container')
+const boxContainer = document.querySelector('.box-container')
 
 for (let i = 1; i <= 100; i++); {
+  const box = document.createElement ('div')
+  box.classList.add('box');
+  box.append(i);
   
+  boxContainer.append(box)
 }
