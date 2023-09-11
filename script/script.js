83c1e80bf4647bb079a09c8f906be0f67d87d6c1
VikVanDik <95vlc17@gmail.com>
2023-09-11 16:16:53 +0200

    [fix] : bug i

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
 const boxContainer = document.querySelector('.box-container')
 
-for (let i = 1; i <= 100; i++); {
+for (let i = 1; i <= 100; i++) {
   const box = document.createElement ('div')
   box.classList.add('box');
   box.append(i);
   
   boxContainer.append(box)
 }
+
