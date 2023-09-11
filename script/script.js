785447514c62458e98b4e3aa7baa2c4de02e98ae
VikVanDik <95vlc17@gmail.com>
2023-09-11 17:04:09 +0200

    [add]: if logic

--- script/script.js
+++ script/script.js
@@ -2,9 +2,21 @@ const boxContainer = document.querySelector('.box-container')
 
 for (let i = 1; i <= 100; i++) {
   const box = document.createElement ('div')
-  box.classList.add('box');
-  box.append(i);
-  
   boxContainer.append(box)
+  
+  if (i % 5 === 0 && i % 3 === 0) {
+    box.classList.add ('buzz-fizz','box')
+    box.append('Buzz-Fizz')
+  }else if(i % 3 === 0){
+    box.classList.add ('fizz','box')
+    box.append('Fizz')
+  }else if(i % 5 === 0){
+    box.classList.add ('buzz','box')
+    box.append('Buzz')
+  }else if(i % 3 != 0 && i % 5 != 0) {
+    box.classList.add('box');
+    box.append(i);
+  }
+  
 }
 
